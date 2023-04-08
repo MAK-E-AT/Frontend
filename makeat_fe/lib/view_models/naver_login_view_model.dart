46a5cbbd9bd752cd674830a5dc7043930c4e3f1c
hardy716 <iabc716@icloud.com>
2023-04-09 00:01:26 +0900

    feat: 네이버 쇼셜 로그인 AOS 인증 추가 2023. 04. 09 1. 안드로이드 네이버 리디렉트 URI 설정 2. 플랫폼에 따라 Redirect URI 설정

--- makeat_fe/lib/view_models/naver_login_view_model.dart
+++ makeat_fe/lib/view_models/naver_login_view_model.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -5,16 +7,29 @@ import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 
 class NaverLoginViewModel extends ChangeNotifier {
+
+  // 플랫폼에 따른 Redirect URI 설정
+  String getRedirectUri() {
+    if (Platform.isIOS) {
+      return dotenv.env['NAVER_IOS_REDIRECT_URI']!;
+    } else if (Platform.isAndroid) {
+      return dotenv.env['NAVER_AOS_REDIRECT_URI']!;
+    } else {
+      throw UnsupportedError("");
+    }
+  }
   
   Future<void> loginWithNaver() async { 
     final clientId = dotenv.env['NAVER_CLIENT_ID']!;
-    final redirectUri = dotenv.env['NAVER_IOS_REDIRECT_URI']!;
+    final redirectUri = getRedirectUri();
     // final clientSecret = dotenv.env['NAVER_CLIENT_SECRET']!;
 
     final url =
         'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=$clientId&redirect_uri=$redirectUri&state=flutter_naver_login';
+    
     final flutterWebviewPlugin = FlutterWebviewPlugin();
 
+    // 플러터 웹뷰 실행
     flutterWebviewPlugin.launch(
       url,
       clearCache: true,
@@ -22,11 +37,17 @@ class NaverLoginViewModel extends ChangeNotifier {
       withJavascript: true,
     );
 
+    // onUrlChanged 이벤트
     flutterWebviewPlugin.onUrlChanged.listen((String url) {
+
+      // 네이버 소셜 로그인 인증 성공
       if (url.startsWith('$redirectUri?code=')) {
         flutterWebviewPlugin.close();
+        
         final authCode = url.split('code=')[1];
         print('NAVER 인증 코드는 $authCode 입니다.');
+        
+      // 네이버 소셜 로그인 인증 실패
       } else {
         print('NAVER/인증 코드가 반환되지 않았습니다.');
       }
