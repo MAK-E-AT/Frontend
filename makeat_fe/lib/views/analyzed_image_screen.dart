2858309d0e3c4323d311a1fde9827757426db160
xman227 <x22z@naver.com>
2023-04-14 20:37:20 +0900

    ui:검색된 음식목록 확인페이지\n 1. 구현 \n 2. 이미지 컨테이너에 항상 꽉차도록 수정 필\n 3. 네비게이션 바 구현 필 \n 4. 검색목록에 없는 음식이 있을 때 버튼 구현 필

--- makeat_fe/lib/views/analyzed_image_screen.dart
+++ makeat_fe/lib/views/analyzed_image_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:makeat_fe/widgets/custom_check_box.dart';
 
 import 'package:makeat_fe/widgets/custom_elevated_button.dart';
 
@@ -31,40 +30,115 @@ class _AnalyzedImageScreenState extends State<AnalyzedImageScreen> {
                     const SizedBox(
                       height: 30.0,
                     ),
-
-                    /* 이미지 슬라이더 
-                  - imagePaths, 초기 인덱스 0
-                  - JumpingDotEffect 애니메이션
-                */
-
-                    /* 키 
-                  1. 숫자 타입
-                  2. 300 이하
-                  3. 소수점은 한 자리까지만 허용
-                */
-
-                    /* 체중 
-                  1. 숫자 타입
-                  2. 200 이하
-                  3. 소수점은 한 자리까지만 허용
-                */
-
-                    /* 이용약관 및 개인정보처리방침 확인 */
-
-                    const CustomCheckBox(
-                      checkBoxText1: '이용약관',
-                      checkBoxText2: ' 및 ',
-                      checkBoxText3: '개인정보처리방침',
-                      checkBoxText4: '에 동의합니다.',
-                      checkBoxColor: Colors.black,
-                      checkBoxIcon: Icons.check_rounded,
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black54, width: 2),
+                      ),
+                      width: MediaQuery.of(context).size.width * 0.76,
+                      height: MediaQuery.of(context).size.height *
+                          0.368, // size 수정 필요
+                      child: Image.asset(
+                          'assets/images/sample_data/sample_food.png'),
                     ),
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(
+                          border: Border(
+                              bottom:
+                                  BorderSide(color: Colors.black54, width: 2))),
+                      child: const Text(
+                        "검색된 음식 목록",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.22,
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          food_info_textbox(
+                              foodName: '계란샐러드', quantity: '1', unit: '인분'),
+                          food_info_textbox(
+                              foodName: '소고기 안심', quantity: '200', unit: 'g'),
+                          food_info_textbox(
+                              foodName: '방울토마토', quantity: '3', unit: '개'),
+                          food_info_textbox(
+                              foodName: '한현호', quantity: '0.5', unit: '공기'),
+                        ],
+                      ),
+                    ),
+                    const TextButton(
+                      onPressed: null, // null값을 주면 비활성화 된다.
+
+                      child: Text('검색 목록에 없는 음식이 있어요...'),
+                    )
                   ],
                 ),
               ),
             ),
             bottomNavigationBar: const CustomElevatedButton(
-              buttonText: '시  작  하  기',
+              buttonText: '영양 성분 확인하기',
             )));
+  }
+}
+
+class food_info_textbox extends StatelessWidget {
+  final String foodName;
+  final String quantity;
+  final String unit;
+
+  const food_info_textbox({
+    Key? key,
+    required this.foodName,
+    required this.quantity,
+    required this.unit,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.black87, width: 1),
+      ),
+      width: MediaQuery.of(context).size.width * 0.7,
+      height: MediaQuery.of(context).size.height * 0.05,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 80,
+            child: Text(
+              foodName,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(
+            width: 70,
+            child: Text(
+              quantity,
+              style: const TextStyle(fontWeight: FontWeight.w700),
+              textAlign: TextAlign.end,
+            ),
+          ),
+          SizedBox(
+            width: 70,
+            child: Text(
+              unit,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
