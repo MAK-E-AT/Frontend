191d85d45f8c986aee1b254c531639190c6c91e7
hardy716 <iabc716@icloud.com>
2023-05-02 22:16:09 +0900

    feat: iOS 사진 가져오기 기능 및 예외처리 2023. 05. 02 1. 갤러리에서 사진 선택하기 2. 카메라로 사진 촬영하기 3. 사진을 가져오는 것을 취소한 경우 예외 처리

--- makeat_fe/lib/views/send_image_to_flask_screen.dart
+++ makeat_fe/lib/views/send_image_to_flask_screen.dart
@@ -2,62 +2,45 @@
 
 import 'dart:io';
 import 'dart:typed_data';
-import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:makeat_fe/widgets/custom_app_bar.dart';
 
-import '../common/no_animation_page_route.dart';
-import '../widgets/custom_elevated_button.dart';
-import 'profile_user_info_screen.dart';
+import '../widgets/custom_bottom_navigation_bar.dart';
+import 'package:http/http.dart' as http;
 
 
-class SendImageToFlask extends StatefulWidget {
-  const SendImageToFlask({super.key});
+class SendPhotoScreen extends StatefulWidget {
+  const SendPhotoScreen({super.key});
 
   @override
-  State<SendImageToFlask> createState() => _SendImageToFlaskState();
+  State<SendPhotoScreen> createState() => _SendPhotoScreenState();
 }
 
-class _SendImageToFlaskState extends State<SendImageToFlask> {
-  late Future<Widget> photo;
+class _SendPhotoScreenState extends State<SendPhotoScreen> {
+  late Future<Uint8List> photo;
   File? mPhoto;
 
   final url = Uri.parse('http://127.0.0.1:9900/test');
 
-  Future<Widget> onPhoto(ImageSource source) async {
+  Future<Uint8List> onPhoto(ImageSource source) async {
     XFile? f = await ImagePicker().pickImage(source: source);
-    mPhoto = File(f!.path);
+    mPhoto = File(f!.path); // cache 에 저장되는 이미지 경로
 
-    Future<Uint8List> sendPhoto = mPhoto!.readAsBytes();
+    Future<Uint8List> photo = mPhoto!.readAsBytes(); // 이미지를 int 값으로 변환
 
-    sendPhoto.then((val) {
-      print('{this.source} 일단 감');
-      print(val);
-      //이게 플라스크한테 넘어가는거
-      //_postRequest(kakaoId: widget.userID, dataByte: val);
-      //   print('지금 백엔드에 넣어봄');
+    photo.then((val) {
+       print(' 데이터 크기 = ${val.length}');
+     }).catchError((error) {});
 
-      //   Future<int> data = _uploadToSignedURL(
-      //       file: val, //val,
-      //       url:
-      //           'https://makeat.s3.amazonaws.com/3.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA37IAGAIXQ52EVAUK%2F20230224%2Fap-northeast-2%2Fs3%2Faws4_request&X-Amz-Date=20230224T044146Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=4c2038c1c2e64589295642eccd3a9acef9ee1889d3aa751cd7af570657b603a8');
-      //   print('전송 성공');
-
-      //   data.then((value) => print('이유는 $value'));
-      // }).catchError((error) {
-      //   print('Faliure to 사진전송');
-    });
-
-    Widget photo = (mPhoto != null) ? Image.file(mPhoto!) : const Text('EMPTY');
     return photo;
-    //setState(() => mPhoto = File(f!.path));
   }
 
   @override
   Widget build(BuildContext context) {
-  
-  return Scaffold(
+    return Scaffold(
+      appBar: const CustomAppBar(),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Center(
@@ -65,12 +48,6 @@ class _SendImageToFlaskState extends State<SendImageToFlask> {
               children: [
                 const SizedBox(
                   height: 20.0,
-                ),
-
-                /* MAK-E-AT 로고(소) */
-                Image.asset(
-                  width: 140.0,
-                  'assets/images/makeat_logo/logo_small_size.png',
                 ),
                 const SizedBox(
                   height: 30.0,
@@ -84,88 +61,72 @@ class _SendImageToFlaskState extends State<SendImageToFlask> {
                   height:
                       MediaQuery.of(context).size.height * 0.38, // size 수정 필요
                   child: Image.asset(
-                      'assets/images/sample_data/sample_food.png',
+                      'assets/images/sample_food/sample_food.png',
                       fit: BoxFit.cover),
                 ),
+
+                // buttton part //////////
                 Container(
                   padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  const Color.fromARGB(255, 167, 212, 41)),
-                          onPressed: () => {
-                                print('텍스트로 추가'),
-                              },
-                          child: const Text('텍스트',
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontFamily: "Pura"))),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 167, 212, 41)),
+                        onPressed: () => {},
+                        child: const Text(
+                          '텍스트',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
                       ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  const Color.fromARGB(255, 13, 48, 78)),
-                          onPressed: () => {
-                                photo = onPhoto(ImageSource.gallery),
-                                print(photo),
-                                print('사진 전송'),
-                                getflask(url),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 13, 48, 78)),
+                        onPressed: () => {
+                          photo = onPhoto(ImageSource.gallery),
+                          photo.then((val) {
+                             print('get image');
+                             print(val);
+                           }).catchError((error) {
+                             print('not get image');
+                           }),
+                          //getflask(url),
 
-                                // photo.then((val) {
-                                //   // 데이터가 나오면 해당 값을 출력
-                                //   Navigator.push(
-                                //       context,
-                                //       MaterialPageRoute(
-                                //           builder: (context) =>
-                                //               AnaPage(mPhoto: val)));
-                                //   // _uploadToSignedURL(
-                                //   //     file: val, url: '1'); //버킷에 업로드 어케 함?
-                                // }).catchError((error) {
-                                //   // error가 해당 에러를 출력
-                                //   print('error: $error');
-                                // }),
-                                // // Navigator.push(
-                                // //     context,
-                                // //     MaterialPageRoute(
-                                // //         builder: (context) => AnaPage(mPhoto: photo)))
-                              },
-                          child: const Text('갤러리',
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontFamily: "Pura"))),
+                          // photo.then((val) {
+                        },
+                        child: const Text(
+                          '갤러리',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
                       ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           backgroundColor: Colors.white,
                         ),
                         onPressed: () {
                           photo = onPhoto(ImageSource.camera);
-
-                          // photo.then((val) {
-                          //   // 데이터가 나오면 해당 값을 출력
-                          //   Navigator.push(
-                          //       context,
-                          //       MaterialPageRoute(
-                          //           builder: (context) => AnaPage(mPhoto: val)));
-                          // }).catchError((error) {
-                          //   // error가 해당 에러를 출력
-                          //   print('error: $error');
-                          // });
-
-                          // Navigator.push(
-                          //     context,
-                          //     MaterialPageRoute(
-                          //         builder: (context) => AnaPage(mPhoto: photo)));
+                          photo.then((val) {
+                             print('get image');
+                             print(val);
+                           }).catchError((error) {
+                             print('not get image');
+                           });
                         },
                         child: const Text(
                           '카메라',
                           style: TextStyle(
-                              fontSize: 18,
-                              color: Color.fromARGB(255, 13, 48, 78),
-                              fontFamily: "Pura"),
+                            fontSize: 18,
+                            color: Color.fromARGB(255, 13, 48, 78),
+                          ),
                         ),
                       )
                     ],
@@ -182,21 +143,11 @@ class _SendImageToFlaskState extends State<SendImageToFlask> {
           ),
         ),
       ),
-      bottomNavigationBar: CustomElevatedButton(
-        buttonText: '영양 성분 확인하기',
-        onPressed: () {
-          return Navigator.push(
-            context, 
-            NoAnimationPageRoute(
-              builder: (context) => const ProfileUserInfoScreen(), 
-              settings: const RouteSettings(name: 'profile_user_info_screen')
-            )
-          );
-        },
-      )
+      bottomNavigationBar: const CustomBottomNavigationBar(),
     );
   }
-}
+} 
+
 
 void getflask(url) async {
   try {
@@ -208,3 +159,4 @@ void getflask(url) async {
     print('실패');
   }
 }
+
